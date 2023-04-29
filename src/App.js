623d589bd5ade6d230beb2b9267d56a05d923124
daniel <danielrubensdaniel@gmail.com>
2023-04-29 03:04:41 -0300

    feat: render map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Map, {Marker} from 'react-map-gl';
+import MAPBOX_TOKEN from './mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 
 function App() {
+  const latitude = -9.184663
+  const longitude= -43.05188941
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      initialViewState={{ latitude, longitude, zoom: 11 }}
+      style={{width: 1200, height: 700}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={longitude} latitude={latitude} color="red" />
+    </Map>
   );
 }
 
---
+++ src/mapbox/index.js
@@ -0,0 +1,3 @@
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGFuaWVscnViZW5zIiwiYSI6ImNsaDFraHNpNDA4Y2gzbG9oeTVzNzQ3dWMifQ.-I4f1fVt3QiRNWQBFklDww'
+
+export default MAPBOX_TOKEN
