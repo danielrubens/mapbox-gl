b486c6fad56cae04251266584129576055e913d7
daniel <danielrubensdaniel@gmail.com>
2023-04-29 18:50:53 -0300

    chore: toast container

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './index.css';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
 
