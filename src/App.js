f3ee132ce35639961d1223eda5a4526318603da5
daniel <danielrubensdaniel@gmail.com>
2023-04-29 20:09:29 -0300

    feat: line drawning polygon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useCallback} from 'react'
 import './App.css'
-import Map, { Marker } from 'react-map-gl'
+import Map, { Source, Layer } from 'react-map-gl'
 import MAPBOX_TOKEN from './mapbox'
 import 'mapbox-gl/dist/mapbox-gl.css'
 // import * as turf from '@turf/turf'
@@ -8,9 +8,37 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 
 function App () {
   const latitude = -9.184663
+  const latitude2 = -9.214663
+  const latitude3 = -9.234663
+
   const longitude = -43.05188941
+  const longitude2 = -43.07188941
+  const longitude3 = -43.09188941
+
   // const GEOFENCE = turf.circle([longitude, latitude], 5, {units: 'miles'})
   const zoom = 11
+
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {type: 'Feature', geometry: {type: 'Polygon', coordinates: [
+        [
+          [longitude, latitude], [longitude2, latitude2], [longitude3, latitude3], [longitude, latitude]
+        ]
+      
+      ]
+      }}
+    ]
+  }
+
+  const layerStyle = {
+    id: 'line',
+    type: 'line',
+    paint: {
+
+      'line-color': '#007cbf'
+    }
+  }
 
   const [latInput, setLatInput] = useState('')
   const [lonInput, setLonInput] = useState('')
@@ -44,7 +72,10 @@ function App () {
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
-      <Marker longitude={longitude} latitude={latitude} color="red" />
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer {...layerStyle}/>
+      </Source>
+      {/* <Marker longitude={longitude} latitude={latitude} color="red" /> */}
     </Map>
     <form >
       <input type="text" placeholder={'Type latitude'} 
