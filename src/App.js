c1c6d43e005dc6dc955623267a2be67a866c88de
daniel <danielrubensdaniel@gmail.com>
2023-04-29 18:46:34 -0300

    feat: goHome button

--- src/App.js
+++ src/App.js
@@ -3,23 +3,38 @@ import './App.css'
 import Map, { Marker } from 'react-map-gl'
 import MAPBOX_TOKEN from './mapbox'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import * as turf from '@turf/turf'
+// import * as turf from '@turf/turf'
 
 
 function App () {
   const latitude = -9.184663
   const longitude = -43.05188941
-  const GEOFENCE = turf.circle([longitude, latitude], 5, {units: 'miles'})
+  // const GEOFENCE = turf.circle([longitude, latitude], 5, {units: 'miles'})
   const zoom = 11
+
+  const [latInput, setLatInput] = useState('')
+  const [lonInput, setLonInput] = useState('')
+  const [invalidInputs, setInvalidInputs] = useState(false)
   const [viewState, setViewState] = useState({longitude, latitude, zoom})
 
   const onMove = useCallback(({viewState}) => {
     const newCenter = [viewState.longitude, viewState.latitude]
-    if (turf.booleanPointInPolygon(newCenter, GEOFENCE)){
+    // if (turf.booleanPointInPolygon(newCenter, GEOFENCE)){
       setViewState(newCenter)
-    }
+    // }
   }, [])
 
+  const search = () => {
+    if(typeof latInput === 'number' && typeof lonInput === 'number'){
+      setViewState({latitude: latInput, longitude: lonInput, zoom})
+      setInvalidInputs(false)
+    } else{
+      setInvalidInputs(true)
+    }
+  }
+  
+  const goHome = () => {setViewState({latitude, longitude, zoom})}
+  
   return (
     <>
     <Map
@@ -31,12 +46,15 @@ function App () {
     >
       <Marker longitude={longitude} latitude={latitude} color="red" />
     </Map>
-    <form>
-      <input type="text" placeholder={'Type longitude'} 
-             onChange={({target}) => setViewState({...viewState, longitude: target.value})} />
+    <form >
       <input type="text" placeholder={'Type latitude'} 
-             onChange={({target}) => setViewState({...viewState, latitude: target.value})}
+             onChange={({target}) => setLatInput(Number(target.value))}
       />
+      <input type="text" placeholder={'Type longitude'} 
+             onChange={({target}) => setLonInput(Number(target.value))} />
+      <button type="button" onClick={search}>Search</button>
+      <button type="ubtton" onClick={goHome}>Go Back Home</button>
+      {invalidInputs && (<p>Invalid Inputs. Type a numeric values for lat and long</p>)}
     </form>
     </>
   )
