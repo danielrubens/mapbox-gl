6774236d57074541107cd1d6f1812166be0b07c2
daniel <danielrubensdaniel@gmail.com>
2023-04-29 17:52:05 -0300

    feat: controlled map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Map, { Marker } from 'react-map-gl'
 import MAPBOX_TOKEN from './mapbox'
@@ -7,9 +7,13 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 function App () {
   const latitude = -9.184663
   const longitude = -43.05188941
+  const zoom = 11
+  const [viewState, setViewState] = useState({longitude, latitude, zoom})
+
   return (
     <Map
-      initialViewState={{ latitude, longitude, zoom: 11 }}
+      {...viewState}
+      onMove={(target) => setViewState(target)}
       style={{ width: 1200, height: 700 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MAPBOX_TOKEN}
