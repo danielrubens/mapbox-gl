44421b158ccf5c179ed86b50e7b0151ab063b8e9
daniel <danielrubensdaniel@gmail.com>
2023-04-29 17:24:33 -0300

    chore: config eslint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Map, {Marker} from 'react-map-gl';
-import MAPBOX_TOKEN from './mapbox';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import React from 'react'
+import './App.css'
+import Map, { Marker } from 'react-map-gl'
+import MAPBOX_TOKEN from './mapbox'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-
-function App() {
+function App () {
   const latitude = -9.184663
-  const longitude= -43.05188941
+  const longitude = -43.05188941
   return (
     <Map
       initialViewState={{ latitude, longitude, zoom: 11 }}
-      style={{width: 1200, height: 700}}
+      style={{ width: 1200, height: 700 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MAPBOX_TOKEN}
     >
       <Marker longitude={longitude} latitude={latitude} color="red" />
     </Map>
-  );
+  )
 }
 
-export default App;
+export default App
