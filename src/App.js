2ae78bcbf0c4b8170be7d77cd734ddefd2a21c41
daniel <danielrubensdaniel@gmail.com>
2023-04-30 00:26:40 -0300

    move: BaseMapp component into components folder

--- src/App.js
+++ src/App.js
@@ -1,85 +1,12 @@
-import React, {useState, useCallback} from 'react'
+import React from 'react'
 import './App.css'
-import Map, { Source, Layer, Marker } from 'react-map-gl'
-import MAPBOX_TOKEN from './mapbox'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import city from './mapbox/city.json'
-// import * as turf from '@turf/turf'
+import BaseMap from './components/BaseMap'
 
 
 function App () {
-  const latitude = -9.184663
-  const longitude = -43.05188941
-  const zoom = 11
-
-  const geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {type: 'Feature', geometry:
-      {
-        type: city.features[0].geometry.type, 
-        coordinates: city.features[0].geometry.coordinates
-      }
-    }
-    ]
-  }
-
-  const layerStyle = {
-    id: 'line',
-    type: 'line',
-    paint: {
-      'line-color': '#007cbf'
-    }
-  }
-
-  const [latInput, setLatInput] = useState('')
-  const [lonInput, setLonInput] = useState('')
-  const [invalidInputs, setInvalidInputs] = useState(false)
-  const [viewState, setViewState] = useState({longitude, latitude, zoom})
-
-  const onMove = useCallback(({viewState}) => {
-    console.log(city.features[0].geometry.coordinates)
-    const newCenter = [viewState.longitude, viewState.latitude]
-    // if (turf.booleanPointInPolygon(newCenter, GEOFENCE)){
-      setViewState(newCenter)
-    // }
-  }, [])
-
-  const search = () => {
-    if(typeof latInput === 'number' && typeof lonInput === 'number'){
-      setViewState({latitude: latInput, longitude: lonInput, zoom})
-      setInvalidInputs(false)
-    } else{
-      setInvalidInputs(true)
-    }
-  }
-  
-  const goHome = () => {setViewState({latitude, longitude, zoom})}
-  
   return (
     <>
-    <Map
-      {...viewState}
-      onMove={onMove}
-      style={{ width: 1200, height: 700 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={MAPBOX_TOKEN}
-    >
-      <Source id="my-data" type="geojson" data={geojson}>
-        <Layer {...layerStyle}/>
-      </Source>
-      <Marker longitude={longitude} latitude={latitude} color="red" />
-    </Map>
-    <form >
-      <input type="text" placeholder={'Type latitude'} 
-             onChange={({target}) => setLatInput(Number(target.value))}
-      />
-      <input type="text" placeholder={'Type longitude'} 
-             onChange={({target}) => setLonInput(Number(target.value))} />
-      <button type="button" onClick={search}>Search</button>
-      <button type="ubtton" onClick={goHome}>Go Back Home</button>
-      {invalidInputs && (<p>Invalid Inputs. Type a numeric values for lat and long</p>)}
-    </form>
+    <BaseMap/>
     </>
   )
 }
---
+++ src/components/BaseMap.js
@@ -0,0 +1,89 @@
+import React, {useState, useCallback} from 'react'
+import './BaseMap.css'
+import Map, { Source, Layer, Marker } from 'react-map-gl'
+import MAPBOX_TOKEN from '../mapbox'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import city from '../mapbox/city.json'
+// import * as turf from '@turf/turf'
+
+
+function BaseMap () {
+  const latitude = -9.184663
+  const longitude = -43.05188941
+  const zoom = 10
+
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {type: 'Feature', geometry:
+      {
+        type: city.features[0].geometry.type, 
+        coordinates: city.features[0].geometry.coordinates
+      }
+    }
+    ]
+  }
+
+  const layerStyle = {
+    id: 'line',
+    type: 'line',
+    paint: {
+      'line-color': '#007cbf'
+    }
+  }
+
+  const [latInput, setLatInput] = useState('')
+  const [lonInput, setLonInput] = useState('')
+  const [invalidInputs, setInvalidInputs] = useState(false)
+  const [viewState, setViewState] = useState({longitude, latitude, zoom})
+
+  const onMove = useCallback(({viewState}) => {
+    console.log(city.features[0].geometry.coordinates)
+    const newCenter = [viewState.longitude, viewState.latitude]
+    // if (turf.booleanPointInPolygon(newCenter, GEOFENCE)){
+      setViewState(newCenter)
+    // }
+  }, [])
+
+  const search = () => {
+    if(typeof latInput === 'number' && typeof lonInput === 'number'){
+      setViewState({latitude: latInput, longitude: lonInput, zoom})
+      setInvalidInputs(false)
+    } else{
+      setInvalidInputs(true)
+    }
+  }
+  
+  const goHome = () => {setViewState({latitude, longitude, zoom})}
+  
+  return (
+    <>
+    <Map
+      {...viewState}
+      onMove={onMove}
+      style={{ width: 1200, height: 700 }}
+      // mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
+      mapStyle="mapbox://styles/mapbox/outdoors-v12"
+      // mapStyle='mapbox://styles/mapbox/light-v11'
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer {...layerStyle}/>
+      </Source>
+      <Marker longitude={longitude} latitude={latitude} color="red" />
+    </Map>
+    <form >
+      <input type="text" placeholder={'Type latitude'} 
+             onChange={({target}) => setLatInput(Number(target.value))}
+      />
+      <input type="text" placeholder={'Type longitude'} 
+             onChange={({target}) => setLonInput(Number(target.value))} />
+      <button type="button" onClick={search}>Search</button>
+      <button type="ubtton" onClick={goHome}>Go Back Home</button>
+      {invalidInputs && (<p>Invalid Inputs. Type a numeric values for lat and long</p>)}
+    </form>
+    </>
+  )
+}
+
+export default BaseMap
